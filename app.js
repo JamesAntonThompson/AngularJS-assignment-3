58a712c1949e50f87283e9fb74d9080b743b5548
James Thompson <jamesantonthompson@gmail.com>
2016-10-03 12:18:42 +1100

    better filter + passing results back to controller working

--- app.js
+++ app.js
@@ -6,26 +6,19 @@ angular.module('NarrowItDownApp', [])
 .service('MenuSearchService', MenuSearchService)
 .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
 
-MenuItemsSearchController.$inject = ['MenuSearchService'];
-function MenuItemsSearchController(MenuSearchService) {
-	console.log('MenuItemsSearchController()');
-	var menu = this;
-
-	menu.searchTerm = "";
+MenuItemsSearchController.$inject = ['MenuSearchService', '$timeout'];
+function MenuItemsSearchController(MenuSearchService, $timeout) {
+	var list = this;
+	//console.log('MenuItemsSearchController()');
 	
-	// var promise = MenuSearchService.getMenuCategories();
 
-	// promise.then(function (response) {
-	// 	menu.categories = response.data;
-	// })
-	// .catch(function (error) {
-	// 	console.log("Something went terribly wrong.");
-	// });
-
-	menu.logMenuItems = function (shortName) {
-		console.log('MenuItemsSearchController.logMenuItems()');
-		var promise = MenuSearchService.getMatchedMenuItems(shortName);
-
+	list.showItems = function () {
+		//console.log('MenuItemsSearchController.showItems()');
+		var promise = MenuSearchService.getMatchedMenuItems( list.searchTerm );
+		promise.then( 	function (response) {
+							console.log('all good');
+							console.log(response);
+						});
 	};
 }
 
@@ -35,59 +28,21 @@ MenuSearchService.$inject = ['$http', 'ApiBasePath']
 function MenuSearchService($http, ApiBasePath) {
 	console.log('MenuSearchService()');
 	var service = this;
-	var matched_item_list = [];
 
 	//Service method
 	service.getMatchedMenuItems = function(searchTerm) {
 		// retrieve the menu items via $http
-		console.log('MenuSearchService.getMatchedMenuItems(' + searchTerm + ')');
-		var response = $http({
-				method: "GET",
-				url: (ApiBasePath + "/menu_items.json")
-			})
-			.then(	function( response ) { 
-						console.log( 'all is good' );
-						//console.log( response.data.menu_items );
-						//console.log( response.data.menu_items.length);
-						for (var i = 0; i < response.data.menu_items.length; i++) {
-						 	var description = response.data.menu_items[i].description;
-						 	//console.log(description);
-						 	if (description.toLowerCase().indexOf(searchTerm) !== -1) {
-						 		// A Match!, so add it to the matched list
-						 		//console.log('A match!');
-						 		matched_item_list.push(response.data.menu_items[i]);
-							} else {
-								//console.log('No match');
-							}
-    					}
-    					return matched_item_list;
-					},
-					function( error ) {
-						console.log( error );
+		//console.log('MenuSearchService.getMatchedMenuItems(' + searchTerm + ')');
+		return $http({ method: "GET", url: (ApiBasePath + "/menu_items.json")})
+			.then (	function( response ) {
+				//console.log(response.data['menu_items'].length);
+				return response.data['menu_items'].filter(
+					function (item) { 
+						return item.description.indexOf(searchTerm) !== -1;
 					}
-			);
+				);
+			});
 	};
-	// Service method - getMenuItems
-	// service.getMenuItems = function () {
-	// 	var response = $http({
-	// 		method: "GET",
-	// 		url: (ApiBasePath + "/menu_items.json")
-	// 	});
-	// 	return response;
-	// };
-
-	// service.getMenuForCategory = function (shortName) {
-	// 	var response = $http({
-	// 		method: "GET",
-	// 		url: (ApiBasePath + "/menu_items.json"),
-	// 		params: {
-	// 			category: shortName
-	// 		}
-	// 	});
-
-	// 	return response;
-	// };
-
 }
 
 })();
