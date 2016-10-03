6e5250564347167b9c0a530a9a153722210e504e
James Thompson <jamesantonthompson@gmail.com>
2016-10-03 13:03:45 +1100

    custom directive working - submission #1

--- app.js
+++ app.js
@@ -4,38 +4,62 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItems);
+
+
+function FoundItems() {
+  var ddo = {
+    templateUrl: 'foundList.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    },
+  };
+
+  return ddo;
+}
+
+
+
 
 NarrowItDownController.$inject = ['MenuSearchService', '$timeout'];
 function NarrowItDownController(MenuSearchService, $timeout) {
 	var list = this;
-	//console.log('NarrowItDownController()');
-	
 
 	list.showItems = function () {
-		//console.log('NarrowItDownController.showItems()');
-		var promise = MenuSearchService.getMatchedMenuItems( list.searchTerm );
-		promise.then( 	function (response) {
-							console.log('all good');
-							console.log(response);
-						});
-	};
+		list.searchTerm = list.searchTerm.trim()
+		if ( list.searchTerm == '' ) {
+			// The search term text box is empty
+			console.log('blank search term');
+			list.found = [];
+		} else {
+			console.log('proper search term');
+			//console.log('NarrowItDownController.showItems()');
+			var promise = MenuSearchService.getMatchedMenuItems( list.searchTerm );
+			promise.then( 	function (response) {
+								console.log(response);
+								list.found = response;
+							});
+		};
+	}
+
+	list.removeItem = function(index) {
+		list.found.splice(index, 1);
+	}
 }
 
 
 // MenuSearch Service 
 MenuSearchService.$inject = ['$http', 'ApiBasePath']
 function MenuSearchService($http, ApiBasePath) {
-	console.log('MenuSearchService()');
 	var service = this;
 
 	//Service method
 	service.getMatchedMenuItems = function(searchTerm) {
 		// retrieve the menu items via $http
-		//console.log('MenuSearchService.getMatchedMenuItems(' + searchTerm + ')');
 		return $http({ method: "GET", url: (ApiBasePath + "/menu_items.json")})
 			.then (	function( response ) {
-				//console.log(response.data['menu_items'].length);
 				return response.data['menu_items'].filter(
 					function (item) { 
 						return item.description.indexOf(searchTerm) !== -1;
